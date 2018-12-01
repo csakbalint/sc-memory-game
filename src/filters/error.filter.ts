9836c630196f7ad22eb50808378718da426f486b
csakbalint <csak.balint@gmail.com>
2018-12-01 22:39:37 +0100

    :bug: add default handling for non-assertion errors

--- src/filters/error.filter.ts
+++ src/filters/error.filter.ts
@@ -1,14 +1,31 @@
-import { ExceptionFilter, Catch, ArgumentsHost } from '@nestjs/common';
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
 import { AssertionError } from 'assert';
 
 @Catch()
 export class ErrorFilter implements ExceptionFilter {
-  catch(error: Error, host: ArgumentsHost) {
-    let response = host.switchToHttp().getResponse();
+  catch(error: HttpException | AssertionError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const request = ctx.getRequest();
+    const response = ctx.getResponse();
 
     if (error instanceof AssertionError) {
-      return response.status(400).send({
+      return response.status(400).json({
+        statusCode: 400,
+        timestamp: new Date().toISOString(),
+        path: request.url,
         message: error.message,
+      });
+    } else {
+      return response.status(error.getStatus()).json({
+        statusCode: error.getStatus(),
+        timestamp: new Date().toISOString(),
+        path: request.url,
+        message: error.message.message,
       });
     }
   }
